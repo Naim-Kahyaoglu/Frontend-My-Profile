3a61a4166d95c15d14580de8ba829f024acb8d26
Naim-Kahyaoglu <naimtrainingweb753@gmail.com>
2024-12-03 17:33:36 +0300

    tailwind kuruldu-eski css tailwind formatına çevrildi-şu an çalışıyor

---
+++ 12/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tailwind'e hangi dosyalarda stil uygulanacağını belirtiyoruz.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
